150121dcce5f187b6dc45d2d2267463efa03b70f
Kripa Jayakumar <kripa.jay@gmail.com>
2021-08-02 21:50:37 +0530

    Added CORS

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require("cors");
 const authenticationHandler = require("./middlewares/authentication");
 const logHandler = require("./middlewares/logger");
 const home = require("./routes/home");
 const bears = require("./routes/bears");
 const app = express();
 
+app.use(cors()); // to stop cors errors
 app.use(express.json()); // Parse JSON objects in req
 app.use(logHandler);
 app.use(authenticationHandler);
@@ -17,6 +19,6 @@ mongoose
     .then(()=>console.log("Connected to db successfully ...."))
     .catch((err)=> console.log("Error has occured while connecting to db:", err));
 
-app.listen(3000, () => {
-    console.log("Listening on Port 3000")
+app.listen(5000, () => {
+    console.log("Listening on Port 5000")
 });
